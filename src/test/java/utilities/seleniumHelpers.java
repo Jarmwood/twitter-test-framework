db3afe66b6dedbe3032bfe313f66de16234afbc7
Jarmwood000 <johntayarmwood@gmail.com>
2024-10-24 20:19:36 -0400

    changed flow to fit java 23

--- src/test/java/utilities/seleniumHelpers.java
+++ src/test/java/utilities/seleniumHelpers.java
@@ -1,115 +1,97 @@
-import org.openqa.selenium.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.testEngine;
 
-import java.time.Duration;
-import java.util.Date;
+/**
+ * Method to enter text in a text field
+ *
+ * @param ele
+ * @param value
+ */
+public static void enterText(WebElement ele, String value) {
+    ele.clear();
+    ele.sendKeys(value);
+}
 
-import static utilities.testEngine.*;
+/**
+ * method to click on an element
+ *
+ * @param ele
+ */
+public static void click(WebElement ele) {
+    ele.click();
+}
 
-public class seleniumHelpers {
-    /**
-     * Method to select an element from a dropdown
-     *
-     * @param ele
-     * @param option
-     */
-    public void selectDropdown(WebElement ele, String option) {
-        Select options = new Select(ele);
-        options.selectByVisibleText(option);
+/**
+ * javaScriptExecutor method to click on an element
+ * @param element
+ * @throws Exception
+ */
+public static void safeJavaScriptClick(WebElement element) throws Exception {
+    try {
+
+        if (element.isEnabled() && element.isDisplayed()) {
+            System.out.println("Clicking on element with using java script click");
+
+            WebDriver driver = testEngine.getDriver();
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+        } else {
+            System.out.println("Unable to click on element");
+        }
+    } catch (Exception e) {
+        System.out.println("Unable to click on element " + e.getStackTrace());
     }
 }
 
-    /**
-     * Method to enter text in a text field
-     *
-     * @param ele
-     * @param value
-     */
-    public static void enterText(WebElement ele, String value) {
-        ele.clear();
-        ele.sendKeys(value);
+
+/**
+ * method to wait for all elements on a web page to load
+ */
+public static void waitUntilVisible() {
+    WebDriverWait wait;
+    wait = new WebDriverWait(testEngine.getDriver(), Duration.ofSeconds(5));
+    wait.until(ExpectedConditions.visibilityOfAllElements());
+}
+
+
+/**
+ * method to wait for a particular web element to load
+ *
+ * @param ele
+ */
+public static void waitUntil(WebElement ele) {
+    WebDriverWait wait;
+    wait = new WebDriverWait(testEngine.getDriver(), Duration.ofSeconds(5));
+    try {
+        wait.until(ExpectedConditions.elementToBeClickable(ele));
+    } catch (Exception e) {
+        e.getStackTrace();
     }
+}
 
-    /**
-     * method to click on an element
-     *
-     * @param ele
-     */
-    public static void click(WebElement ele) {
-        ele.click();
-    }
+/**
+ * method to capture a page title
+ */
+public static void capturePageTitle() {
+    testEngine.getDriver().getTitle();
+}
 
-    /**
-     * javaScriptExecutor method to click on an element
-     *
-     * @param element
-     * @throws Exception
-     */
-    public static void safeJavaScriptClick(WebElement element) throws Exception {
-        try {
+/**
+ * method to capture text from a page
+ *
+ * @param ele
+ * @return
+ */
+public static String captureTextValue(WebElement ele) {
 
-            if (element.isEnabled() && element.isDisplayed()) {
-                System.out.println("Clicking on element with using java script click");
+    String textValue = ele.getText();
 
-                WebDriver driver = testEngine.getDriver();
-                ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
-            } else {
-                System.out.println("Unable to click on element");
-            }
-        } catch (Exception e) {
-            System.out.println("Unable to click on element "+ e.getStackTrace());
-        }
-    }
+    return textValue;
 
-
-    /**
-     * method to wait for all elements on a web page to load
-     */
-    public static void waitUntilVisible() {
-        WebDriverWait wait;
-        wait = new WebDriverWait(testEngine.getDriver(), Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.visibilityOfAllElements());
-    }
-
-
-    /**
-     * method to wait for a particular web element to load
-     *
-     * @param ele
-     */
-    public static void waitUntil(WebElement ele) {
-        WebDriverWait wait;
-        wait = new WebDriverWait(testEngine.getDriver(), Duration.ofSeconds(5));
-        try {
-            wait.until(ExpectedConditions.elementToBeClickable(ele));
-        } catch (Exception e) {
-            e.getStackTrace();
-        }
-    }
-
-    /**
-     * method to capture a page title
-     */
-    public static void capturePageTitle() {
-        testEngine.getDriver().getTitle();
-    }
-
-    /**
-     * method to capture text from a page
-     *
-     * @param ele
-     * @return
-     */
-    public static String captureTextValue(WebElement ele) {
-
-        String textValue = ele.getText();
-
-        return textValue;
-
-    }
+}
 
 /**
  * Method to return the current Time as
@@ -120,21 +102,18 @@ public String getCurrentTime() {
     String fileName = myDate.toString().replace(":", "_").replace(" ", "_");
     String day = fileName.substring(0, 10);
     String time = fileName.substring(11, 19);
-    String [] x = time.split("_");
+    String[] x = time.split("_");
     String AMPM = "";
     int hour = Integer.parseInt(x[0]);
-    if(hour >= 12) {
+    if (hour >= 12) {
         AMPM = "PM";
-        hour = hour-12;
-    }else {
+        hour = hour - 12;
+    } else {
         AMPM = "AM";
     }
-    return day+ hour+ x[1] + AMPM;
+    return day + hour + x[1] + AMPM;
 }
 
 public void main() {
 }
-
-
-
 
